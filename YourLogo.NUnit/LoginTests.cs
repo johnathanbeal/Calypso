b403f4ab17f00bc4e5df39db647d2fa81e66bbb0
Johnathan Beal <johnathanbeal@gmail.com>
2020-07-29 07:34:01 -0400

    first test

--- YourLogo.NUnit/LoginTests.cs
+++ YourLogo.NUnit/LoginTests.cs
@@ -1,18 +1,49 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+//using SeleniumExtras.WaitHelpers;
+using System.IO;
+using System.Threading;
+using System.Runtime;
 
 namespace YourLogo.NUnit
 {
     public class LoginTests
     {
+        IWebDriver driver;
+
         [SetUp]
-        public void Setup()
+        public void Before()
         {
+            driver = new ChromeDriver(Path.GetFullPath(@"../../../../" + "_drivers"));
+        }
+
+        [TearDown]
+        public void After()
+        {
+
         }
 
         [Test]
         public void BasicLoginTest()
         {
-            Assert.Fail();
+            driver.Url = "http://automationpractice.com/index.php?";
+
+            driver.FindElement(By.CssSelector("a[href='http://automationpractice.com/index.php?controller=my-account']")).Click();
+            
+            WebDriverWait waiter = new WebDriverWait(driver, System.TimeSpan.FromSeconds(90));
+            waiter.Until(ExpectedConditions.ElementExists(By.CssSelector("#email")));
+            waiter.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("#email")));
+            Thread.Sleep(1000);
+
+            IWebElement emailAddressField = driver.FindElement(By.CssSelector("#email"));
+                        
+            Assert.That(emailAddressField.Displayed);
+            Assert.That(emailAddressField.Enabled);
+
+            
+
         }
     }
 }
---
+++ YourLogo.NUnit/PurchaseTests.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+
+namespace YourLogo.NUnit
+{
+    public class PurchaseTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ImpulsePurchase()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void SmallPurchase()
+        {
+            Assert.Fail();
+        }
+
+        [Test]
+        public void LargePurchase()
+        {
+            Assert.Fail();
+        }
+    }
+}
